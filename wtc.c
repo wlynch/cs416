182ef77ad04364adceb936659dac97505a0c48bd
Karina Ruzinov <kruzinova@gmail.com>
2013-01-29 16:05:50 -0500

    near final wtc

--- wtc.c
+++ wtc.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "wtc_proc.h"
+
 void print_usage() {
-  fprintf(stderr, "usage: wtc <input file>\n");
+  fprintf(stderr, "usage: wtc <method> <input file>\n");
   exit(0);
 }
 
-void print_adjacency_matrix(int * E, int number_of_vertices) {
+void print_adjacency_matrix(int * T, int number_of_vertices) {
   int i, j;
   for (i = 0; i < number_of_vertices; i++) {
     for (j = 0; j < number_of_vertices; j++) {
-      printf("%i ", E[i + j * number_of_vertices]);
+      printf("%i ", T[i + j * number_of_vertices]);
     }
     printf("\n");
   }
@@ -23,14 +25,14 @@ int main(int argc, char ** argv) {
 
   /* the vertices graph, length number_of_vertices * number_of_vertices 
    * to find a connection you look at E[x + y * number_of_vertices] */
-  int * E;
+  int * T;
 
-  if (argc != 2) {
+  if (argc != 3) {
     print_usage();
   }
 
   /* load the input file */
-  input_fd = fopen(argv[1], "r");
+  input_fd = fopen(argv[2], "r");
   if (!input_fd) {
     fprintf(stderr, "cannot open input file %s\n", argv[1]);
     exit(1);
@@ -41,15 +43,34 @@ int main(int argc, char ** argv) {
   printf("%i processes\n%i vertices\n", number_of_processes, number_of_vertices);
 
   /* initialize E, the vertex graph*/
-  E = calloc(number_of_vertices * number_of_vertices, 0);
+  T = calloc(number_of_vertices * number_of_vertices, 0);
 
   while ( fscanf(input_fd, "%i %i", &i, &j) == 2 ) {
       printf("(%i, %i)\n", i, j);
-      E[i-1 + (j-1)*number_of_vertices] = 1;
+      T[(i-1) + (j-1)*number_of_vertices] = 1;
   }
 
   /* print the graph */
-  print_adjacency_matrix(E, number_of_vertices);
+  print_adjacency_matrix(T, number_of_vertices);
+
+  /* determine which method to use to solve the transitive graph */
+  switch (argv[1][0] - '0') {
+    case 1:
+
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    default:
+      fprintf(stderr, "invalid method %s\n", argv[1]);
+      break;
+  }
+
+  /* clean up */
+  free(T);
 
   /* close the input file */
   fclose(input_fd);
