532e6b0c59a575e2f78d5d768db2e4e24f796f6c
Billy Lynch <wlynch92@gmail.com>
2013-01-30 18:09:31 -0500

    minimal skeleton of wtc_thr

---
+++ wtc_thr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "wtc_thr.h"
+
+void wtc_thr_init(int * T, size_t size){
+
+}
+
+void wtc_thr(){
+
+}
+
+void wtc_thr_destroy(){
+
+}
---
+++ wtc_thr.h
@@ -0,0 +1,8 @@
+#ifndef _WTC_THR_H
+#define _WTC_THR_H
+
+void wtc_thr_init(int * T, size_t size);
+void wtc_thr();
+void wtc_thr_destroy();
+
+#endif
