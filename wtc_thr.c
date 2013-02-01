5d86be22820a6dab28867f2acf4d9798acb86257
Jonathan Maltz <jm1271@gmail.com>
2013-02-01 13:05:10 -0500

    we can't use any of the fancy tricks in the other algorithm :( reverted to the version in the pdf

--- wtc_thr.c
+++ wtc_thr.c
@@ -7,7 +7,9 @@
 
 #include "wtc_thr.h"
 
-int *transitive_closure;
+int *odd_closure;
+int *even_closure;
+int **both_closures;
 size_t size_of_graph;
 size_t number_of_vertices;
 
@@ -21,8 +23,19 @@ void print_array();
 void wtc_thr_init(int * T, size_t num_vertices){
 	size_of_graph = num_vertices * num_vertices * sizeof(int);
 	number_of_vertices = num_vertices;
-	transitive_closure = (int *)malloc(size_of_graph);
-	memcpy(transitive_closure, T, size_of_graph);
+
+    //allocate both transitive closure objects
+	odd_closure = (int *)malloc(size_of_graph);
+	even_closure = (int *)malloc(size_of_graph);
+	
+    //copy the current graph into both even and odd closures
+    memcpy(odd_closure, T, size_of_graph);
+	memcpy(even_closure, T, size_of_graph);
+   
+    //initialize both transitive closures
+    both_closures = (int**)malloc(sizeof(int*) * 2);
+    both_closures[0] = even_closure;
+    both_closures[1] = odd_closure;
 
 }
 
@@ -30,22 +43,30 @@ void wtc_thr_init(int * T, size_t num_vertices){
  * Actually run warshal's algorithm
  */
 int *wtc_thr(){
+
+    int even_closure_loc, odd_closure_loc;
 	int k, i, j, index;
 
-
 	//hold each vertex steady
-	for( k = 0; k < number_of_vertices; k++ ) {
-		for( i = 0; i < number_of_vertices; i++ ) {
-			if(transitive_closure[get_array_loc(i, k)]){
-				for( j = 0; j < number_of_vertices; j++ ) {
-					index = get_array_loc(i, j);
-					transitive_closure[index] = transitive_closure[index] | transitive_closure[get_array_loc(k, j)]; 
-					
-				}
-			}
-		}
-	}
-	return transitive_closure;
+	for( k = 0; k < number_of_vertices; k++ ) 
+    {
+        //figure out which closure we are reading and which we are writing
+        int *closure_writing = k % 2 == 0 ? even_closure : odd_closure;
+        int *closure_reading = k % 2 == 1 ? even_closure : odd_closure;
+
+		for( i = 0; i < number_of_vertices; i++ ) 
+        {
+            for( j = 0; j < number_of_vertices; j++ ) 
+            {
+                index = get_array_loc(i, j);
+                closure_writing[index] = closure_reading[index] | (closure_reading[get_array_loc(i, k)] & closure_reading[get_array_loc(k, j)]); 
+                
+            }
+        }
+   	}
+   
+    //return the most recently written array
+    return k % 2 == 0 ? odd_closure : even_closure;
 }
 
 /*
@@ -55,12 +76,12 @@ int get_array_loc(int x, int y){
 	return ((x * number_of_vertices)) + y;
 }
 
-void print_array()
+void print_array(int *array)
 {
 		int i;
 		for(i = 0; i < number_of_vertices * number_of_vertices; i++)
 		{
-				printf("%d ", transitive_closure[i]);
+		    printf("%d ", array[i]);
 		}
 		printf("\n");
 }
@@ -69,5 +90,7 @@ void print_array()
  * Frees all memory allocated by this process
  */
 void wtc_thr_destroy(){
-	free(transitive_closure);
+	free(even_closure);
+    free(odd_closure);
+    free(both_closures);
 }
