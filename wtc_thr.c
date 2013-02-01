109212a3c73aac19e388c1bfba9a9cdf7008d65a
Jonathan Maltz <jm1271@gmail.com>
2013-01-31 19:55:18 -0500

    Added all the code necessary to get the algorithm working single threaded.  Still need to test it

--- wtc_thr.c
+++ wtc_thr.c
@@ -7,14 +7,48 @@
 
 #include "wtc_thr.h"
 
-void wtc_thr_init(int * T, size_t size){
+int *transitive_closure;
+size_t size_of_graph;
+size_t number_of_vertices;
 
+int get_array_loc(int, int);
+
+/*
+ * Create any global structures which are necessary
+ *
+ */
+void wtc_thr_init(int * T, size_t num_vertices){
+	size_of_graph = num_vertices * num_vertices * sizeof(int);
+	number_of_vertices = num_vertices;
+	transitive_closure = (int *)malloc(size_of_graph);
+	memcpy(transitive_closure, T, size_of_graph);
 }
 
-void wtc_thr(){
+/*
+ * Actually run warshal's algorithm
+ */
+int *wtc_thr(){
+	int k, i, j, index;
 
+	//hold each vertex steady
+	for( k = 1; k < number_of_vertices; k++ ) {
+		for( i = 1; i < number_of_vertices; i++ ) {
+			for( j = 1; j < number_of_vertices; j++ ) {
+				index = get_array_loc(i, j);
+				transitive_closure[index] = transitive_closure[index] | (transitive_closure[get_array_loc(i,k)] & transitive_closure[get_array_loc(k,j)]);
+			}
+		}
+	}
+	return transitive_closure;
+}
+
+/*
+ * Finds the corresponding 2D array location in our 1D array
+ */
+int get_array_loc(int x, int y){
+	return ((x * number_of_vertices) - 1) + y - 1;
 }
 
 void wtc_thr_destroy(){
-
+	free(transitive_closure);
 }
--- wtc_thr.h
+++ wtc_thr.h
@@ -2,7 +2,7 @@
 #define _WTC_THR_H
 
 void wtc_thr_init(int * T, size_t size);
-void wtc_thr();
+int *wtc_thr();
 void wtc_thr_destroy();
 
 #endif
