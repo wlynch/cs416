c555ed6e86222bbe73251ae197e9c16297843796
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-02-04 23:14:57 -0500

    yay, all that code I wrote is equivalent to shmat and friends

--- wtc_proc.c
+++ wtc_proc.c
@@ -3,78 +3,37 @@
 #include <string.h>
 
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <sys/mman.h>
-#include <errno.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #include <semaphore.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include "wtc_proc.h"
 
 /* shared vertices graph, will result in the transitive closure graph */
 int * T;
 sem_t * T_sem;
-struct shared_memory * T_shared_memory;
-struct shared_memory * T_sem_shared_memory;
-#define shared_memory_path "/tmp/wtc_proc_shared4"
-#define T_sem_shared_memory_path "/tmp/wtc_proc_t_sem_sadnkasnd"
 
-struct shared_memory * share_memory(char * path, size_t size) {
-  int shared_fd;
-  void * map;
-
-  shared_fd = shm_open(path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-  if (shared_fd == -1) {
-    perror("cannot open shared memory"); exit(1);
-  }
-
-  if (ftruncate(shared_fd, size) == -1) {
-    perror("cannot truncate shared memory fd");
-    goto failure;
-  }
-
-  map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shared_fd, 0);
-  if (map == MAP_FAILED) {
-    perror("could not mmap");
-    goto failure;
-  }
-
-  struct shared_memory * shm = malloc(sizeof(struct shared_memory));
-  shm->address = map;
-  shm->fd = shared_fd;
-  shm->size = size;
-
-  return shm;
-  failure:
-  close(shared_fd);
-  shm_unlink(path); exit(1);
+int AllocateSharedMemory(int n) {
+  return shmget(IPC_PRIVATE, n, IPC_CREAT | SHM_R | SHM_W);
 }
 
-void unshare_memory(struct shared_memory * memory, char * path) {
-  int ret = munmap(memory->address, memory->size);
-  if (ret == -1) {
-    perror("munmap"); exit(1);
-  }
-
-  ret = close(memory->fd);
-  if (ret == -1) {
-    perror("close of shared_memory_fd"); exit(1);
-  }
-
-  ret = shm_unlink(path);
-  if (ret == -1) {
-    perror("shm_unlink failed. should change shared_memory_path"); exit(1);
-  }
+void* MapSharedMemory(int id) {
+  void* addr;
+  addr = shmat(id, NULL, 0); /* Attach the segment... */
+  shmctl(id, IPC_RMID, NULL); /* ...and mark it destroyed. */
+  return addr;
 }
 
 void wtc_proc_init(int * initial_matrix, int n, int number_of_processes) {
-  T_shared_memory = share_memory(shared_memory_path, sizeof(int) * n * n);
-  T = T_shared_memory->address;
+  T = MapSharedMemory(AllocateSharedMemory(sizeof(int) * n * n));
 
-  T_sem_shared_memory = share_memory(T_sem_shared_memory_path, sizeof(sem_t));
-  T_sem = T_shared_memory->address;
+  T_sem = MapSharedMemory(AllocateSharedMemory(sizeof(sem_t)));
   sem_init(T_sem, 1, number_of_processes);
 
   memcpy(T, initial_matrix, sizeof(int) * n * n);
@@ -82,13 +41,24 @@ void wtc_proc_init(int * initial_matrix, int n, int number_of_processes) {
 
 int * wtc_proc(int n) {
   int i,j,k;
+  int pid;
 
   for (k = 0; k < n; k++) { /* for each vertex */
-
     for (i = 0 ; i < n; i++) { /* for each row */
       if (T[k + i*n]) { /* optimization, check the first of the row */
-        for (j = 0; j < n; j++) { /* for each column */
-          T[j + i*n] = T[j + i*n] | (T[j + k*n] & T[k + i*n]);
+        sem_wait(T_sem);
+        pid = fork();
+
+        if (pid == -1) {
+          perror("FFFFUUUUUUU"); exit(1);
+        } else if (pid == 0) {
+          printf("working on row %i\n", i);
+          for (j = 0; j < n; j++) { /* for each column */
+            T[j + i*n] = T[j + i*n] | (T[j + k*n] & T[k + i*n]);
+          }
+          sem_post(T_sem);
+          exit(0);
+        } else {
         }
       }
     }
@@ -98,7 +68,9 @@ int * wtc_proc(int n) {
 }
 
 void wtc_proc_cleanup() {
-  unshare_memory(T_shared_memory, shared_memory_path);
-  unshare_memory(T_sem_shared_memory, T_sem_shared_memory_path);
+  shmdt(T);
+  puts("unlinked T_shared_memory");
+  shmdt(T_sem);
+  puts("unlinked T_sem_shared_memory");
 }
 
--- wtc_proc.h
+++ wtc_proc.h
@@ -1,11 +1,6 @@
 #ifndef _WTC_PROC_H
 #define _WTC_PROC_H
 
-struct shared_memory {
-  void * address;
-  int fd, size;
-};
-
 void wtc_proc_init(int * T, int number_of_vertices, int number_of_processes);
 int * wtc_proc(int n);
 void wtc_proc_cleanup();
