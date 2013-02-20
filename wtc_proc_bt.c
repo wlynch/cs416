4ee1373129f317f7a79b047729e231697038f44c
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-02-19 23:12:52 -0500

    created shared memory spaces for all the shit

--- wtc_proc_bt.c
+++ wtc_proc_bt.c
@@ -22,6 +22,10 @@ sem_t * sem;
 pthread_mutex_t * lock;
 pthread_cond_t * cond;
 
+pthread_mutex_t * row_lock, * other_lock;
+pthread_cond_t * k_cond;
+int * row, * running;
+
 int AllocateSharedMemory(int n) {
   return shmget(IPC_PRIVATE, n, IPC_CREAT | SHM_R | SHM_W);
 }
@@ -53,13 +57,23 @@ void wtc_proc_bt_init(int * initial_matrix, int n, int number_of_processes) {
   lock = give_memory(sizeof(pthread_mutex_t));
   cond = give_memory(sizeof(pthread_cond_t));
 
+  row_lock = give_memory(sizeof(pthread_mutex_t));
+  k_cond = give_memory(sizeof(pthread_cond_t));
+  running = give_memory(sizeof(int));
+  row = give_memory(sizeof(int));
+
+  *row = 0;
+  *running = 1;
+
   pthread_mutexattr_init(&lock_attr);
   pthread_mutexattr_setpshared(&lock_attr, PTHREAD_PROCESS_SHARED);
   pthread_mutex_init(lock, &lock_attr);
+  pthread_mutex_init(row_lock, &lock_attr);
 
   pthread_condattr_init(&cond_attr);
   pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED);
   pthread_cond_init(cond, &cond_attr);
+  pthread_cond_init(k_cond, &cond_attr);
 
   for (process_number = 0; process_number < number_of_processes; process_number++) {
     wtc_proc_create(process_number, number_of_processes, n);
@@ -124,10 +138,16 @@ void wtc_proc_create(int process_number, int number_of_processes, int n) {
 }
 
 void wtc_proc_bt_cleanup() {
-  shmdt(T);
-  shmdt(sem);
   shmdt(lock);
   shmdt(cond);
+  shmdt(sem);
+  shmdt(T);
+
+  shmdt(other_lock);
+  shmdt(row_lock);
+  shmdt(k_cond);
+  shmdt(row);
+
   if (sem_close(sem) == -1) {
     perror("sem_close"); exit(EXIT_FAILURE);
   }
