c39acd1505859ca674c429646f2cf26c731bae62
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-04-23 17:58:46 -0400

    server is stable and I think is waiting for an rpc call

--- p3/serverSNFS/server.c
+++ p3/serverSNFS/server.c
@@ -14,26 +14,78 @@ void print_usage() {
    "-port [port number] -mount [mount location]\n");
 }
 
+void ping__reply_to_ping(ProtobufCService * service,
+                                 const Ping * input,
+                                 PingResponse_Closure closure,
+                                 void * closure_data) {
+  // init the message
+  PingResponse ping_response = PING_RESPONSE__INIT;
+  void * ping_response_buf;
+  unsigned ping_response_buf_length;
+
+  // set the reply string
+  ping_response.reply = strdup("hi");
+
+  // get the lenght of the response and malloc some space
+  ping_response_buf_length = ping_response__get_packed_size(&ping_response);
+  ping_response_buf = malloc(ping_response_buf_length);
+
+  // pack it into the buffer
+  ping_response__pack(&ping_response, ping_response_buf);
+
+  // respond with the ping_response buffer
+  closure(&ping_response_buf, closure_data);
+}
+
+static PingService_Service ping_service = PING_SERVICE__INIT(ping__);
+
 int main(int argc, char **argv) {
+  ProtobufC_RPC_Server * server;
+  const char * name = malloc(6);
+
+  struct server_param * params = setup(argc, argv);
+  if (!params) {
+    fprintf(stderr, "could not parse params\n");
+  } else {
+    sprintf(name, "%d", params->port);
+  }
+
+  printf("Starting server, mount: %s, port: %i\n", params->mount, params->port);
+
+  server = protobuf_c_rpc_server_new (PROTOBUF_C_RPC_ADDRESS_TCP, name, (ProtobufCService *) &(ping_service), NULL);
+
+  if (!server) {
+    perror("server: ");
+    return 1;
+  }
+
+  for (;;)
+    protobuf_c_dispatch_run (protobuf_c_dispatch_default ());
+
+  perror("dispatch: ");
+
+  return 0;
+}
+
+struct server_param * setup(int argc, char ** argv) {
   int parsed_error;
   struct server_param * params = malloc(sizeof(struct server_param));
 
   if(argc != 5) {
     print_usage();
-    return 1;
+    free(params);
+    return 0;
   }
 
   parsed_error = parse_server_params(argc, argv, params);
 
   if(parsed_error) {
     printf("Error, unable to parse your input arguments, " \
-      "please only use -mount and -port arguments\n");
-    return 5;
+           "please only use -mount and -port arguments\n");
+    return 0;
   }
 
-  printf("Starting server, mount: %s, port: %i\n", params->mount, params->port);
-
-  return 0;
+  return params;
 }
 
 int parse_server_params(int argc, char ** argv, struct server_param * param) {
--- p3/serverSNFS/server.h
+++ p3/serverSNFS/server.h
@@ -6,4 +6,5 @@ struct server_param {
 };
 
 void print_usage();
+struct server_param * setup(int argc, char ** argv);
 int parse_server_params(int argc, char ** argv, struct server_param * param);
