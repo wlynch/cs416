2d1ac1e5319503d07f22365b220edcb9d9413aa2
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-04-22 23:44:24 -0400

    remove xmlrpc stuff, create protobuf message
    removed all of the xmlrpc cruft and built a basic
    program that creates a message and packs it. Next
    is to create the rpc server/client interaction.

--- p3/clientSNFS/client.c
+++ p3/clientSNFS/client.c
@@ -8,101 +8,84 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include <xmlrpc-c/base.h>
-#include <xmlrpc-c/client.h>
-
 #include "client.h"
 
-bool parse_input(char **, char **);
-char *create_addr();
+#include "../protobuf-model/ping.pb-c.h"
+
+/* globals are bad, stop this */
+int port_number;
+char * address, * mount_path;
+
+/* super bad, no no no */
 extern struct fuse_operations ops;
 
-int port_number;
+int main (int argc, char ** argv) {
+  Ping ping = PING__INIT;
+  void * ping_buf;
+  unsigned ping_buf_length;
 
-int main(int argc, char **argv)
-{
-    char * fuse_args[6];
-    bool parsed;
+  // create the message
+  ping.origin = strdup("localhost:8080");
 
-    if(argc != 7)
-    {
-       printf("Error, your input must contain input arguments" \
-                "please supply a -port, -address, and -mount option\n");
-        return 1;
-    }
+  // get it's length and create the buffer it will go into
+  ping_buf_length = ping__get_packed_size(&ping);
+  ping_buf = malloc(ping_buf_length);
 
-    parsed = parse_input(argv, fuse_args);
+  // pack the message into the buffer
+  ping__pack(&ping, ping_buf);
 
-    if(!parsed)
-    {
-        printf("Error, you must supply a -mount, -address, and -port flag\n");
-        return 1;
-    }
+  // send it on it's way
+  fwrite(ping_buf, ping_buf_length, 1, stdout);
 
-    address = create_addr();
-    init_client();
+  free(ping_buf);
+  //return start_fuse(argc, argv);
+}
 
-    /*xmlrpc_client_call2f(&env, client, address, methodName, &result,
-            "(ii)", (xmlrpc_int32) 7, (xmlrpc_int32) 7);
+int start_fuse (int argc, char ** argv) {
+  char * fuse_args[6];
+  bool parsed;
 
-    xmlrpc_read_int(&env, result, &sum);
+  if (argc != 7) {
+    printf("Error, your input must contain input arguments" \
+     "please supply a -port, -address, and -mount option\n");
+    return 1;
+  }
 
-    xmlrpc_DECREF(result);*/
+  parsed = parse_input(argv, fuse_args);
 
-    fuse_args[2] = "-o";
+  if (!parsed) {
+    printf("Error, you must supply a -mount, -address, and -port flag\n");
+    return 1;
+  }
+
+  fuse_args[2] = "-o";
     #ifdef __APPLE__
-        fuse_args[3] = "allow_other";
+  fuse_args[3] = "allow_other";
     #else
-        fuse_args[3] = "user_allow_other";
+  fuse_args[3] = "user_allow_other";
     #endif
-    fuse_args[4] = "-o";
-    fuse_args[5] = "allow_root";
+  fuse_args[4] = "-o";
+  fuse_args[5] = "allow_root";
 
-    return fuse_main(4, fuse_args, &ops, NULL);
+  return fuse_main(4, fuse_args, &ops, NULL);
 }
 
-bool parse_input(char **argv, char **fuse_args)
-{
-    int i;
+bool parse_input (char **argv, char **fuse_args) {
+  int i;
 
-    for(i = 1; i < 7; i += 2)
-    {
-        if(strcmp(argv[i], "-mount") == 0)
-        {
-            fuse_args[0] = "-mount";
-            fuse_args[1] = argv[i + 1];
-            mount_path = argv[i + 1];
-        }
-        else if(strcmp(argv[i], "-port") == 0)
-        {
-            port_number = strtol(argv[i + 1], (char **)NULL, 10);
-        }
-        else if(strcmp(argv[i], "-address") == 0)
-        {
-            address = argv[i + 1];
-        }
-        else
-        {
-            return false;
-        }
+  for (i = 1; i < 7; i += 2) {
+    if (strcmp(argv[i], "-mount") == 0) {
+      fuse_args[0] = "-mount";
+      fuse_args[1] = argv[i + 1];
+      mount_path = argv[i + 1];
+    } else if(strcmp(argv[i], "-port") == 0) {
+      port_number = strtol(argv[i + 1], (char **)NULL, 10);
+    } else if(strcmp(argv[i], "-address") == 0) {
+      address = argv[i + 1];
+    } else {
+      return false;
     }
+  }
 
-    return true;
-}
-
-char *create_addr()
-{
-    char number[7];
-    char *addr = (char *)malloc((strlen(address) + 19)*sizeof(char));
-    addr[0] = '\0';
-
-    sprintf(number, "%d", port_number);
-
-    strcat(addr, "http://");
-    strcat(addr, address);
-    strcat(addr, ":");
-    strcat(addr, number);
-    strcat(addr, "/RPC2");
-
-    return addr;
+  return true;
 }
--- p3/clientSNFS/client.h
+++ p3/clientSNFS/client.h
@@ -1,30 +1,4 @@
-#ifndef CLIENT_H_
-#define CLIENT_H_
+#pragma once
 
-#define CLIENT_NAME "XML-RPC CS416 Test Client"
-#define CLIENT_VERSION "1.0"
-
-
-char *address;
-char *mount_path;
-
-xmlrpc_client *client;
-xmlrpc_env env;
-
-void init_client()
-{    
-    xmlrpc_env_init(&env);
-    xmlrpc_client_setup_global_const(&env);
-    xmlrpc_client_create(&env, XMLRPC_CLIENT_NO_FLAGS, CLIENT_NAME, \
-            CLIENT_VERSION, NULL, 0, &client);
-}
-
-void destroy_client()
-{
-    xmlrpc_env_clean(&env);
-    xmlrpc_client_destroy(client);
-    xmlrpc_client_teardown_global_const();
-}
-
-#endif
-
+int start_fuse (int argc, char ** argv);
+bool parse_input(char **, char **);
